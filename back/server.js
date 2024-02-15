cfd2adf0aee2fcc58222c3789c434343d0b98969
Rajeev Kumar Singh <rajeevkumarsingh5252@gmail.com>
2024-02-15 23:50:09 +0530

    Update server.js

--- back/server.js
+++ back/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chatappfront-phi.vercel.app',
     },
 });
 
